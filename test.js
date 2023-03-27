d48defd6b084f690c4602975aaec2f212fe15116
NgWJ422 <jng010422@gmail.com>
2023-03-27 15:27:13 +0800

    test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+let data1=1
+let data2="1"
+console.log(data1,typeof data1)
+console.log(data2,typeof data2)
+
+let data3 = data1 + parseInt(data2)
+console.log(data1, typeof data3)
+
+user={
+    name:"soo",
+    faculty:"fkekk",
+    hp:{
+        office:"123456",
+        home:"78900"
+    },
+    email:"jhs.gamil.com",
+
+}
+console.log(user.hp.office,typeof user.hp.office)
